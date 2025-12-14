98ec7e467b011f3571cd88ecadfd1aaee4125db4
Ahmed Almashad <ahmedheshamalmashad@gmail.com>
2025-12-14 16:35:51 +0200

    Adding a new line to the code:

--- LED_blink.py
+++ LED_blink.py
@@ -21,6 +21,7 @@ except:
     # You won't get any error messages for debugging  
     # so only use it once your code is working  
     print ("Other error or exception occurred!" )
+    print("I'm youssef!")
   
 finally:  
     GPIO.cleanup() # this ensures a clean exit  
