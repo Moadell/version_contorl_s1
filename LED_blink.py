9d49b013e7205cb0bdc5db3e32e2c0abbfef551f
aliabdullahkandil <162797000+aliabdullahkandil@users.noreply.github.com>
2025-12-14 16:49:26 +0200

    ali

--- LED_blink.py
+++ LED_blink.py
@@ -1,19 +1,12 @@
-#this comment was added by ym
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
+#ALi_this comment was added by ym
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library from time import sleep # Import the sleep function from the time module
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setwarnings(False) # Ignore warning for now GPIO.setmode(GPIO.BOARD) # Use physical pin numbering GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set 
+initial value to low (off)
 
-try:
-    while True: # Run forever
-        GPIO.output(8, GPIO.HIGH) # Turn on
-        sleep(1) # Sleep for 1 second
-        GPIO.output(8, GPIO.LOW) # Turn off
-        sleep(1) # Sleep for 1 second\
+try: while True: # Run forever GPIO.output(8, GPIO.HIGH) # Turn on sleep(1) # Sleep for 1 second GPIO.output(8, GPIO.LOW) # Turn off sleep(1) # Sleep for 1 second\
 
-except KeyboardInterrupt:  
+except KeyboardInterrupt:
     # here you put any code you want to run before the program   
     # exits when you press CTRL+C  
     print('Exit')
